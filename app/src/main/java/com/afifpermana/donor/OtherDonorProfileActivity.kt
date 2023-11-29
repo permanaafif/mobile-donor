ebc9e5fc4725d066b50c4def3023a54334daeaa5
afif permana <afifp04@gmail.com>
2023-11-29 13:18:28 +0700

    menambahkan chek koneksi internet di semua halaman

--- app/src/main/java/com/afifpermana/donor/OtherDonorProfileActivity.kt
+++ app/src/main/java/com/afifpermana/donor/OtherDonorProfileActivity.kt
@@ -32,6 +32,7 @@ import com.afifpermana.donor.service.LaporanAPI
 import com.afifpermana.donor.service.PostAPI
 import com.afifpermana.donor.service.PostFavoriteAPI
 import com.afifpermana.donor.service.ProfileAPI
+import com.afifpermana.donor.util.ConnectivityChecker
 import com.afifpermana.donor.util.Retro
 import com.afifpermana.donor.util.SharedPrefLogin
 import com.airbnb.lottie.LottieAnimationView
@@ -116,103 +117,123 @@ class OtherDonorProfileActivity : AppCompatActivity(), CallBackData {
     }
 
     private fun profileView(id:Int) {
-        val retro = Retro().getRetroClientInstance().create(ProfileAPI::class.java)
-        retro.profileOtherDonor("Bearer ${sharedPref.getString("token")}",id).enqueue(object : Callback<ProfileResponse> {
-            override fun onResponse(
-                call: Call<ProfileResponse>,
-                response: Response<ProfileResponse>
-            ) {
-                val resCode = response.code()
-                if (resCode == 200){
-                    Log.e("profilecaliak","success")
-                    val res = response.body()!!
-                    tv_golongan_darah.text = res.user.id_golongan_darah.nama
-                    if (res.user.total_donor_darah.toString() != "null"
-                        && res.user.total_donor_darah.toString().toInt() >= 0){
+        val connectivityChecker = ConnectivityChecker(this)
+        if (connectivityChecker.isNetworkAvailable()){
+            //koneksi aktif
+            val retro = Retro().getRetroClientInstance().create(ProfileAPI::class.java)
+            retro.profileOtherDonor("Bearer ${sharedPref.getString("token")}",id).enqueue(object : Callback<ProfileResponse> {
+                override fun onResponse(
+                    call: Call<ProfileResponse>,
+                    response: Response<ProfileResponse>
+                ) {
+                    val resCode = response.code()
+                    if (resCode == 200){
+                        Log.e("profilecaliak","success")
+                        val res = response.body()!!
+                        tv_golongan_darah.text = res.user.id_golongan_darah.nama
+                        if (res.user.total_donor_darah.toString() != "null"
+                            && res.user.total_donor_darah.toString().toInt() >= 0){
                             tv_total_donor_darah.text = res.user.total_donor_darah.toString()
-                    }else{
-                        tv_total_donor_darah.text = "0"
-                    }
-                    if(res.user.gambar.isNullOrEmpty()){
-                        fotoProfile.setImageResource(R.drawable.baseline_person_24)
-                    }else{
-                        Picasso.get().load("http://138.2.74.142/images/${res.user!!.gambar}").into(fotoProfile)
-                        val pathFoto = "http://138.2.74.142/images/${res.user!!.gambar}"
-                        fotoProfile.setOnClickListener {
-                            showAlertGambar(pathFoto!!)
+                        }else{
+                            tv_total_donor_darah.text = "0"
                         }
+                        if(res.user.gambar.isNullOrEmpty()){
+                            fotoProfile.setImageResource(R.drawable.baseline_person_24)
+                        }else{
+                            Picasso.get().load("http://138.2.74.142/images/${res.user!!.gambar}").into(fotoProfile)
+                            val pathFoto = "http://138.2.74.142/images/${res.user!!.gambar}"
+                            fotoProfile.setOnClickListener {
+                                showAlertGambar(pathFoto!!)
+                            }
 
+                        }
+                        nama.text = res.user.nama.toString().capitalize()
                     }
-                    nama.text = res.user.nama.toString().capitalize()
+                    else{
+                        sharedPref.setStatusLogin(false)
+                        sharedPref.logOut()
+                        val intent = Intent(this@OtherDonorProfileActivity, LoginActivity::class.java)
+                        startActivity(intent)
+                    }
                 }
-                else{
-                    sharedPref.setStatusLogin(false)
-                    sharedPref.logOut()
-                    val intent = Intent(this@OtherDonorProfileActivity, LoginActivity::class.java)
-                    startActivity(intent)
-                }
-            }
 
-            override fun onFailure(call: Call<ProfileResponse>, t: Throwable) {
-                Toast.makeText(this@OtherDonorProfileActivity,"Gagal", Toast.LENGTH_LONG).show()
-            }
-        })
+                override fun onFailure(call: Call<ProfileResponse>, t: Throwable) {
+                    Toast.makeText(this@OtherDonorProfileActivity,"Gagal", Toast.LENGTH_LONG).show()
+                }
+            })
+        }else{
+            //koneksi tidak aktif
+            connectivityChecker.showAlertDialogNoConnection()
+        }
+
     }
 
     private fun postViewOtherDonor(id: Int) {
         cl_post.visibility = View.VISIBLE
         recyclerView.visibility = View.GONE
         loadingLottie.visibility = View.VISIBLE
-        val retro = Retro().getRetroClientInstance().create(PostAPI::class.java)
-        retro.postOtherDonor("Bearer ${sharedPref.getString("token")}", id).enqueue(object :
-            Callback<List<PostRespone>> {
-            override fun onResponse(
-                call: Call<List<PostRespone>>,
-                response: Response<List<PostRespone>>
-            ) {
-                Log.e("pesanden",response.code().toString())
-                if (response.isSuccessful) {
-                    val res = response.body()
-                    if (res.isNullOrEmpty()){
+        val connectivityChecker = ConnectivityChecker(this)
+        if (connectivityChecker.isNetworkAvailable()){
+            //koneksi aktif
+            val retro = Retro().getRetroClientInstance().create(PostAPI::class.java)
+            retro.postOtherDonor("Bearer ${sharedPref.getString("token")}", id).enqueue(object :
+                Callback<List<PostRespone>> {
+                override fun onResponse(
+                    call: Call<List<PostRespone>>,
+                    response: Response<List<PostRespone>>
+                ) {
+                    Log.e("pesanden",response.code().toString())
+                    if (response.isSuccessful) {
+                        val res = response.body()
+                        if (res.isNullOrEmpty()){
+                            cl_post.visibility = View.VISIBLE
+                            loadingLottie.visibility = View.GONE
+                            nodataLottie.visibility = View.VISIBLE
+                            recyclerView.visibility = View.GONE
+                        }else{
+                            // Menggunakan sortedByDescending untuk mengurutkan berdasarkan tanggal terbaru
+                            for (i in res!!) {
+                                val data = Post(
+                                    i.id.toString().toInt(),
+                                    i.id_pendonor.toString().toInt(),
+                                    i.gambar_profile.toString(),
+                                    i.nama.toString(),
+                                    i.updated_at.toString(),
+                                    i.text.toString(),
+                                    i.gambar.toString(),
+                                    i.jumlah_comment.toString().toInt()
+                                )
+                                newData.add(data)
+                            }
+                            adapter.notifyDataSetChanged()
+                            cl_post.visibility = View.GONE
+                            loadingLottie.visibility = View.VISIBLE
+                            nodataLottie.visibility = View.GONE
+                            recyclerView.visibility = View.VISIBLE
+                        }
+                    }
+                    if(response.code() == 404){
                         cl_post.visibility = View.VISIBLE
                         loadingLottie.visibility = View.GONE
                         nodataLottie.visibility = View.VISIBLE
                         recyclerView.visibility = View.GONE
-                    }else{
-                        // Menggunakan sortedByDescending untuk mengurutkan berdasarkan tanggal terbaru
-                        for (i in res!!) {
-                            val data = Post(
-                                i.id.toString().toInt(),
-                                i.id_pendonor.toString().toInt(),
-                                i.gambar_profile.toString(),
-                                i.nama.toString(),
-                                i.updated_at.toString(),
-                                i.text.toString(),
-                                i.gambar.toString(),
-                                i.jumlah_comment.toString().toInt()
-                            )
-                            newData.add(data)
-                        }
-                        adapter.notifyDataSetChanged()
-                        cl_post.visibility = View.GONE
-                        loadingLottie.visibility = View.VISIBLE
-                        nodataLottie.visibility = View.GONE
-                        recyclerView.visibility = View.VISIBLE
                     }
                 }
-                if(response.code() == 404){
-                    cl_post.visibility = View.VISIBLE
-                    loadingLottie.visibility = View.GONE
-                    nodataLottie.visibility = View.VISIBLE
-                    recyclerView.visibility = View.GONE
-                }
-            }
 
-            override fun onFailure(call: Call<List<PostRespone>>, t: Throwable) {
-                Toast.makeText(this@OtherDonorProfileActivity, t.message.toString(), Toast.LENGTH_SHORT).show()
-                Log.e("pesanden", t.message.toString())
-            }
-        })
+                override fun onFailure(call: Call<List<PostRespone>>, t: Throwable) {
+                    Toast.makeText(this@OtherDonorProfileActivity, t.message.toString(), Toast.LENGTH_SHORT).show()
+                    Log.e("pesanden", t.message.toString())
+                }
+            })
+        }
+        else{
+//            //koneksi tidak aktif
+//            connectivityChecker.showAlertDialogNoConnection()
+            cl_post.visibility = View.VISIBLE
+            loadingLottie.visibility = View.GONE
+            nodataLottie.visibility = View.VISIBLE
+            recyclerView.visibility = View.GONE
+        }
     }
 
     private fun postFavorite() {
@@ -239,17 +260,18 @@ class OtherDonorProfileActivity : AppCompatActivity(), CallBackData {
                         }
                         adapter.notifyDataSetChanged()
                     }
-                }else{
-                    Toast.makeText(this@OtherDonorProfileActivity,"terjaadi kesalahan", Toast.LENGTH_SHORT).show()
                 }
+//                else{
+//                    Toast.makeText(this@OtherDonorProfileActivity,"terjaadi kesalahan", Toast.LENGTH_SHORT).show()
+//                }
             }
 
             override fun onFailure(call: Call<List<PostFavoriteResponse>>, t: Throwable) {
-                Toast.makeText(this@OtherDonorProfileActivity,"Sesi kamu habis", Toast.LENGTH_SHORT).show()
-                sharedPref.logOut()
-                sharedPref.setStatusLogin(false)
-                startActivity(Intent(this@OtherDonorProfileActivity, LoginActivity::class.java))
-                finish()
+//                Toast.makeText(this@OtherDonorProfileActivity,"Sesi kamu habis", Toast.LENGTH_SHORT).show()
+//                sharedPref.logOut()
+//                sharedPref.setStatusLogin(false)
+//                startActivity(Intent(this@OtherDonorProfileActivity, LoginActivity::class.java))
+//                finish()
             }
         })
     }
